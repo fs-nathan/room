6b6c03d548cb52a3353d399d658605f33aba4eb1
fs-nathan <nathan.makeitpro@gmail.com>
2022-06-01 17:26:21 +0700

    Server: update pm2 config

---
+++ server/index.js
@@ -0,0 +1,26 @@
+//Require the express moule
+const express = require("express")
+
+//create a new express application
+const app = express()
+
+//require the http module
+const http = require("http").Server(app)
+
+// require the socket.io module
+const io = require("socket.io")
+
+const port = process.env.PORT || 8000
+
+const socket = io(http)
+//create an event listener
+
+//To listen to messages
+socket.on("connection", (socket) => {
+    console.log("user connected")
+})
+
+//wire up the server to listen to our port process.env.PORT || 8000
+http.listen(port, () => {
+    console.log("Listening port: " + port)
+})
